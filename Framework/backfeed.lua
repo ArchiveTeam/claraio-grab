6d332fe8a15312137190d652799185e2c6bdac4d
OrIdow6 <68304414+OrIdow6@users.noreply.github.com>
2022-12-30 01:19:33 -0800

    Change backfeed base URL again

--- Framework/backfeed.lua
+++ Framework/backfeed.lua
@@ -26,8 +26,8 @@ local queue_list_to = function(list, key)
       local tries = 0
       while tries < 10 do
         local body, code, headers, status = http.request(
-          "https://legacy-api.arpa.li/backfeed/legacy/" .. key .. "/",
-          to_send
+          "https://legacy-api.arpa.li/backfeed/legacy/" .. key,
+          to_send .. "\0"
         )
         if code == 200 or code == 409 then
           break
