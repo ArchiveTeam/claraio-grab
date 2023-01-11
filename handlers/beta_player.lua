9e95012a4a0361158b24770a4b02de50c2085b7a
OrIdow6 <68304414+OrIdow6@users.noreply.github.com>
2023-01-10 20:56:16 -0800

    Allow weird beta player pages

--- handlers/beta_player.lua
+++ handlers/beta_player.lua
@@ -8,8 +8,9 @@ local cur_stat_code = nil
 module.get_urls = function(file, url, is_css, iri)
 	if cur_stat_code == 200 then
 		local res = string.match(get_body(), "xhr%.open%('GET', '(https://clara.io/resources/[a-f0-9]+)'")
-		assert(res)
-		queue_request({url=res}, retry_common.only_retry_handler(10, {200}))
+		if res then
+			queue_request({url=res}, retry_common.only_retry_handler(10, {200}))
+		end
 	end
 end
 
